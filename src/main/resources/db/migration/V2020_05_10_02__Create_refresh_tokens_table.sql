c852bd6feb91c0658b6394a32ba8ebfafb4a1cd0
Brendan Tierney <tiernebre@gmail.com>
2020-05-10 17:32:48 -0600

    many to one relation

--- src/main/resources/db/migration/V2020_05_10_02__Create_refresh_tokens_table.sql
+++ src/main/resources/db/migration/V2020_05_10_02__Create_refresh_tokens_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS refresh_tokens (
   token TEXT PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-  created_at TIMESTAMP NOT NULL DEFAULT now()
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  user_id BIGINT REFERENCES users (id)
 );
