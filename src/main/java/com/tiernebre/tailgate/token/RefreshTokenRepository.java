eb7053d47cbc0508a2179a4f60a000429eb1543d
Brendan Tierney <tiernebre@gmail.com>
2020-05-10 17:37:47 -0600

    refresh token repository interface

---
+++ src/main/java/com/tiernebre/tailgate/token/RefreshTokenRepository.java
@@ -0,0 +1,6 @@
+package com.tiernebre.tailgate.token;
+
+import com.tiernebre.tailgate.repository.Repository;
+
+public interface RefreshTokenRepository extends Repository<RefreshTokenEntity, String> {
+}
