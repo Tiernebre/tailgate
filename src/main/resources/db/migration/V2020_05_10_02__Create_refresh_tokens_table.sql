74a0d5d262debd89031c445c4f4183ce9220413f
Brendan Tierney <tiernebre@gmail.com>
2020-05-10 21:13:54 -0600

    not null enforced user id

--- src/main/resources/db/migration/V2020_05_10_02__Create_refresh_tokens_table.sql
+++ src/main/resources/db/migration/V2020_05_10_02__Create_refresh_tokens_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS refresh_tokens (
   token TEXT PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
   created_at TIMESTAMP NOT NULL DEFAULT now(),
-  user_id BIGINT REFERENCES users (id)
+  user_id BIGINT REFERENCES users (id) NOT NULL
 );
