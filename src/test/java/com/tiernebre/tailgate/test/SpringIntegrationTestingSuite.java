02446bd8df9cc0fdf676f4ef634b0af0e90eb7ce
Brendan Tierney <tiernebre@gmail.com>
2020-05-17 14:16:33 -0600

    spin up database for all integration tests

--- src/test/java/com/tiernebre/tailgate/test/SpringIntegrationTestingSuite.java
+++ src/test/java/com/tiernebre/tailgate/test/SpringIntegrationTestingSuite.java
@@ -1,9 +1,7 @@
 package com.tiernebre.tailgate.test;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("h2")
 @SpringBootTest
-public abstract class SpringIntegrationTestingSuite {
+public abstract class SpringIntegrationTestingSuite extends DatabaseIntegrationTestSuite {
 }
