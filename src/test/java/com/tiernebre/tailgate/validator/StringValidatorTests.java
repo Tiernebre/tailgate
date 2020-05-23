5f8d1f5155ee5ba570a37a8616d84df338fe2a72
Brendan Tierney <tiernebre@gmail.com>
2020-05-22 20:36:31 -0600

    test blank string validation

--- src/test/java/com/tiernebre/tailgate/validator/StringValidatorTests.java
+++ src/test/java/com/tiernebre/tailgate/validator/StringValidatorTests.java
@@ -3,6 +3,8 @@ package com.tiernebre.tailgate.validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
@@ -12,10 +14,24 @@ public class StringValidatorTests {
     @DisplayName("requireNonBlank")
     public class RequireNonBlankTests {
         @Test
-        @DisplayName("throws StringIsBlankExcception with provided message if the string to validate is null")
+        @DisplayName("throws StringIsBlankException with provided message if the string to validate is null")
         public void throwsStringIsBlankExceptionWithProvidedMessageIfTheStringToValidateIsNull() {
             String expectedMessage = "Expected Test Failure.";
             StringIsBlankException thrownException = assertThrows(StringIsBlankException.class, () -> StringValidator.requireNonBlank(null, expectedMessage));
+            assertEquals(expectedMessage, thrownException.getMessage());
+        }
+
+        @ParameterizedTest(name = "throws StringIsBlankException with provided message if the string to validate is equal to \"{0}\"")
+        @ValueSource(strings = {
+                "",
+                " ",
+                "  ",
+                "    ",
+                "                             "
+        })
+        public void throwsStringIsBlankExceptionWithProvidedMessageIfTheStringToValidateIs(String stringToValidate) {
+            String expectedMessage = "Expected Test Failure.";
+            StringIsBlankException thrownException = assertThrows(StringIsBlankException.class, () -> StringValidator.requireNonBlank(stringToValidate, expectedMessage));
             assertEquals(expectedMessage, thrownException.getMessage());
         }
     }
