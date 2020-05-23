eea2678751072d7fe4dff12d47a82bb6baf32c66
Brendan Tierney <tiernebre@gmail.com>
2020-05-22 20:41:25 -0600

    null source

--- src/test/java/com/tiernebre/tailgate/validator/StringValidatorTests.java
+++ src/test/java/com/tiernebre/tailgate/validator/StringValidatorTests.java
@@ -2,8 +2,8 @@ package com.tiernebre.tailgate.validator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.Assert.assertEquals;
@@ -14,22 +14,17 @@ public class StringValidatorTests {
     @Nested
     @DisplayName("requireNonBlank")
     public class RequireNonBlankTests {
-        @Test
-        @DisplayName("throws StringIsBlankException with provided message if the string to validate is null")
-        public void throwsStringIsBlankExceptionWithProvidedMessageIfTheStringToValidateIsNull() {
-            String expectedMessage = "Expected Test Failure.";
-            StringIsBlankException thrownException = assertThrows(StringIsBlankException.class, () -> StringValidator.requireNonBlank(null, expectedMessage));
-            assertEquals(expectedMessage, thrownException.getMessage());
-        }
-
         @ParameterizedTest(name = "throws StringIsBlankException with provided message if the string to validate is equal to \"{0}\"")
-        @ValueSource(strings = {
-                "",
-                " ",
-                "  ",
-                "    ",
-                "                             "
-        })
+        @ValueSource(
+                strings = {
+                        "",
+                        " ",
+                        "  ",
+                        "    ",
+                        "                             "
+                }
+        )
+        @NullSource
         public void throwsStringIsBlankExceptionWithProvidedMessageIfTheStringToValidateIs(String stringToValidate) {
             String expectedMessage = "Expected Test Failure.";
             StringIsBlankException thrownException = assertThrows(StringIsBlankException.class, () -> StringValidator.requireNonBlank(stringToValidate, expectedMessage));
