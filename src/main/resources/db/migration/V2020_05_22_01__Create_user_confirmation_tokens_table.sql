a02d54dd7b98b67abbcff3ec28d22740f1972b80
Brendan Tierney <tiernebre@gmail.com>
2020-05-22 19:33:42 -0600

    naming update

--- src/main/resources/db/migration/V2020_05_22_01__Create_user_confirmation_tokens_table.sql
+++ src/main/resources/db/migration/V2020_05_22_01__Create_user_confirmation_tokens_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS invite_tokens (
+CREATE TABLE IF NOT EXISTS user_confirmation_tokens (
   token TEXT PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
   created_at TIMESTAMP NOT NULL DEFAULT now(),
   user_id BIGINT REFERENCES users (id) NOT NULL UNIQUE
