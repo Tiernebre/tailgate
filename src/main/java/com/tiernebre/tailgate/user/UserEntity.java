b59ef3196211e6a8137df63e2f2bdcaf32f5a865
Brendan Tierney <tiernebre@gmail.com>
2020-05-22 19:41:10 -0600

    add created at to user entity

--- src/main/java/com/tiernebre/tailgate/user/UserEntity.java
+++ src/main/java/com/tiernebre/tailgate/user/UserEntity.java
@@ -3,10 +3,13 @@ package com.tiernebre.tailgate.user;
 import lombok.Builder;
 import lombok.Value;
 
+import java.time.Instant;
+
 @Value
 @Builder
 public class UserEntity {
     Long id;
     String email;
     String password;
+    Instant createdAt;
 }
