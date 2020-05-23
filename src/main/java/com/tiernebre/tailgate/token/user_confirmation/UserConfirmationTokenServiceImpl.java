c718053ac15464fa3b10626864e893c21d833ed4
Brendan Tierney <tiernebre@gmail.com>
2020-05-22 20:04:51 -0600

    service implementation

---
+++ src/main/java/com/tiernebre/tailgate/token/user_confirmation/UserConfirmationTokenServiceImpl.java
@@ -0,0 +1,10 @@
+package com.tiernebre.tailgate.token.user_confirmation;
+
+import com.tiernebre.tailgate.user.UserDto;
+
+public class UserConfirmationTokenServiceImpl implements UserConfirmationTokenService {
+    @Override
+    public String createOneForUser(UserDto user) {
+        return null;
+    }
+}
