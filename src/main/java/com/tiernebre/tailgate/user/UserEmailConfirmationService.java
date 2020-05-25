abb2918f2600279c21f0103677eab1489b845a7d
Brendan Tierney <tiernebre@gmail.com>
2020-05-25 00:11:59 -0600

    user confirmation service

---
+++ src/main/java/com/tiernebre/tailgate/user/UserEmailConfirmationService.java
@@ -0,0 +1,8 @@
+package com.tiernebre.tailgate.user;
+
+public class UserEmailConfirmationService implements UserConfirmationService {
+    @Override
+    public void sendOne(UserDto userToConfirm) {
+
+    }
+}
