eeebe38a7833739c0398a9a85fa8d3e02fa7f039
Brendan Tierney <tiernebre@gmail.com>
2020-06-03 22:09:16 -0600

    get from working on the response

--- src/test/java/com/tiernebre/tailgate/test/email/MailhogItem.java
+++ src/test/java/com/tiernebre/tailgate/test/email/MailhogItem.java
@@ -3,8 +3,24 @@ package com.tiernebre.tailgate.test.email;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class MailhogItem {
-    @JsonProperty(value = "ID")
+    @JsonProperty("ID")
     private String id;
+    @JsonProperty("From")
+    private MailhogItemContact from;
+
+    @Data
+    private static class MailhogItemContact {
+        @JsonProperty("Relays")
+        private Set<String> relays;
+        @JsonProperty("Mailbox")
+        private String mailbox;
+        @JsonProperty("Domain")
+        private String domain;
+        @JsonProperty("Params")
+        private String params;
+    }
 }
