33f7388bc2af1c6047edbab71611034c47549efb
Brendan Tierney <tiernebre@gmail.com>
2020-06-03 20:52:16 -0600

    mailhog rest configuration

---
+++ src/test/java/com/tiernebre/tailgate/test/email/MailhogRestConfiguration.java
@@ -0,0 +1,16 @@
+package com.tiernebre.tailgate.test.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@Configuration
+public class MailhogRestConfiguration {
+    @Bean
+    public RestTemplate mailhogRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:8025/api/v2"));
+        return restTemplate;
+    }
+}
