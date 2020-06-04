00c35c7d30fc968e64f38aa9c28372fb98ef18eb
Brendan Tierney <tiernebre@gmail.com>
2020-06-03 23:20:59 -0600

    just use a mock bean

--- src/test/java/com/tiernebre/tailgate/user/UserServiceImplIntegrationTests.java
+++ src/test/java/com/tiernebre/tailgate/user/UserServiceImplIntegrationTests.java
@@ -7,12 +7,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@MockBeans({
+        @MockBean(UserConfirmationService.class)
+})
 public class UserServiceImplIntegrationTests extends DatabaseIntegrationTestSuite {
     @Autowired
     private UserService userService;
