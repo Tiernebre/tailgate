ce52fe576f906815ba1eb96ea203802af739ca93
Brendan Tierney <tiernebre@gmail.com>
2020-06-04 20:18:59 -0600

    User Confirmation is Asynchronous (#24)
    * create initial async bean
    
    * setup working service annotation
    
    * test around async wrapper
    
    * make async primary

---
+++ src/main/java/com/tiernebre/tailgate/user/AsyncUserEmailConfirmationService.java
@@ -0,0 +1,19 @@
+package com.tiernebre.tailgate.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Primary
+public class AsyncUserEmailConfirmationService implements UserConfirmationService {
+    private final UserEmailConfirmationService userEmailConfirmationService;
+
+    @Override
+    @Async
+    public void sendOne(UserDto userToConfirm) {
+        userEmailConfirmationService.sendOne(userToConfirm);
+    }
+}
---
+++ src/test/java/com/tiernebre/tailgate/user/AsyncUserEmailConfirmationServiceTests.java
@@ -0,0 +1,34 @@
+package com.tiernebre.tailgate.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class AsyncUserEmailConfirmationServiceTests {
+    @Mock
+    private UserEmailConfirmationService userEmailConfirmationService;
+
+    @InjectMocks
+    private AsyncUserEmailConfirmationService asyncUserEmailConfirmationService;
+
+    @Nested
+    @DisplayName("sendOne")
+    public class SendOneTests {
+        @Test
+        @DisplayName("asynchronously sends user confirmation")
+        public void asynchronouslySendsUserConfirmation() {
+            UserDto userDto = UserFactory.generateOneDto();
+            asyncUserEmailConfirmationService.sendOne(userDto);
+            verify(userEmailConfirmationService, times(1)).sendOne(eq(userDto));
+        }
+    }
+}
