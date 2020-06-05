6d4e4e508fd7ce7bff238876c56ebb4d41ce4dc6
Brendan Tierney <tiernebre@gmail.com>
2020-06-04 21:35:22 -0600

    initial jooq repo implementation

---
+++ src/main/java/com/tiernebre/tailgate/security_questions/SecurityQuestionJooqRepository.java
@@ -0,0 +1,10 @@
+package com.tiernebre.tailgate.security_questions;
+
+import java.util.List;
+
+public class SecurityQuestionJooqRepository implements SecurityQuestionRepository {
+    @Override
+    public List<SecurityQuestionEntity> getAll() {
+        return null;
+    }
+}
