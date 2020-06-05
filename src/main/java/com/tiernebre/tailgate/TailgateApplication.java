de0af603abca71690d1700a67ac380ba12fee708
Brendan Tierney <tiernebre@gmail.com>
2020-06-04 20:07:00 -0600

    setup working service annotation

--- src/main/java/com/tiernebre/tailgate/TailgateApplication.java
+++ src/main/java/com/tiernebre/tailgate/TailgateApplication.java
@@ -3,9 +3,11 @@ package com.tiernebre.tailgate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.tiernebre.tailgate")
+@EnableAsync
 public class TailgateApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/tiernebre/tailgate/user/AsyncUserEmailConfirmationService.java
+++ src/main/java/com/tiernebre/tailgate/user/AsyncUserEmailConfirmationService.java
@@ -1,11 +1,17 @@
 package com.tiernebre.tailgate.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AsyncUserEmailConfirmationService implements UserConfirmationService {
-    @Override
-    public void sendOne(UserDto userToConfirm) {
+    private final UserEmailConfirmationService userEmailConfirmationService;
 
+    @Override
+    @Async
+    public void sendOne(UserDto userToConfirm) {
+        userEmailConfirmationService.sendOne(userToConfirm);
     }
 }
