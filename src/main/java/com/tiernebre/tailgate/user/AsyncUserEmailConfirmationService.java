785937d65ca08de57a608ce8f8ede93c3df08281
Brendan Tierney <tiernebre@gmail.com>
2020-06-04 20:04:41 -0600

    create initial async bean

---
+++ src/main/java/com/tiernebre/tailgate/user/AsyncUserEmailConfirmationService.java
@@ -0,0 +1,11 @@
+package com.tiernebre.tailgate.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AsyncUserEmailConfirmationService implements UserConfirmationService {
+    @Override
+    public void sendOne(UserDto userToConfirm) {
+
+    }
+}
