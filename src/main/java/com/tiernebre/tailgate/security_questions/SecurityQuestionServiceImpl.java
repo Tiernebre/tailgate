327bd6f3944db14af450bca5acb6771138b22d17
Brendan Tierney <tiernebre@gmail.com>
2020-06-04 22:21:01 -0600

    initial service impl

---
+++ src/main/java/com/tiernebre/tailgate/security_questions/SecurityQuestionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.tiernebre.tailgate.security_questions;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SecurityQuestionServiceImpl implements SecurityQuestionService {
+    @Override
+    public List<SecurityQuestionDto> getAll() {
+        return null;
+    }
+}
