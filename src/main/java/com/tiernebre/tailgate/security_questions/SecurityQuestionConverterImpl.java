93d8218fb5c5537857251f6cb7f749e2d09e94b8
Brendan Tierney <tiernebre@gmail.com>
2020-06-04 20:59:26 -0600

    setup converter implemntation

--- src/main/java/com/tiernebre/tailgate/security_questions/SecurityQuestionConverterImpl.java
+++ src/main/java/com/tiernebre/tailgate/security_questions/SecurityQuestionConverterImpl.java
@@ -2,16 +2,19 @@ package com.tiernebre.tailgate.security_questions;
 
 import com.tiernebre.tailgate.converter.ConverterImpl;
 
-import java.util.function.Function;
-
 public class SecurityQuestionConverterImpl extends ConverterImpl<SecurityQuestionDto, SecurityQuestionEntity> implements SecurityQuestionConverter {
-    /**
-     * Constructor.
-     *
-     * @param fromDto    Function that converts given dto entity into the domain entity.
-     * @param fromEntity Function that converts given domain entity into the dto entity.
-     */
-    public SecurityQuestionConverterImpl(Function<SecurityQuestionDto, SecurityQuestionEntity> fromDto, Function<SecurityQuestionEntity, SecurityQuestionDto> fromEntity) {
-        super(fromDto, fromEntity);
+    public SecurityQuestionConverterImpl() {
+        super(
+                SecurityQuestionConverterImpl::convertToEntity,
+                SecurityQuestionConverterImpl::convertToDto
+        );
+    }
+
+    private static SecurityQuestionDto convertToDto(SecurityQuestionEntity entity) {
+        return null;
+    }
+
+    private static SecurityQuestionEntity convertToEntity(SecurityQuestionDto dto) {
+        return null;
     }
 }
