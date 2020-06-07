6c4222a906a5b2d938d8ae8f2d6f8867e1f54cac
Brendan Tierney <tiernebre@gmail.com>
2020-06-06 19:35:55 -0600

    setup seucrity questions controller with unit tests

---
+++ src/test/java/com/tiernebre/tailgate/security_questions/SecurityQuestionRestfulControllerTests.java
@@ -0,0 +1,35 @@
+package com.tiernebre.tailgate.security_questions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class SecurityQuestionRestfulControllerTests {
+    @InjectMocks
+    private SecurityQuestionRestfulController securityQuestionRestfulController;
+
+    @Mock
+    private SecurityQuestionService securityQuestionService;
+
+    @Nested
+    @DisplayName("getAll")
+    public class GetAllTests {
+        @Test
+        public void returnsTheGottenSecurityQuestions() {
+            List<SecurityQuestionDto> expectedQuestions = SecurityQuestionFactory.generateMultipleDtos();
+            when(securityQuestionService.getAll()).thenReturn(expectedQuestions);
+            List<SecurityQuestionDto> gottenQuestions = securityQuestionRestfulController.getAll();
+            assertEquals(expectedQuestions, gottenQuestions);
+        }
+    }
+}
