82628b6c8503b1812624958a525d75d2513f1e1c
Brendan Tierney <tiernebre@gmail.com>
2020-06-06 20:05:34 -0600

    test json response body

--- src/test/java/com/tiernebre/tailgate/security_questions/SecurityQuestionRestfulControllerIntegrationTests.java
+++ src/test/java/com/tiernebre/tailgate/security_questions/SecurityQuestionRestfulControllerIntegrationTests.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.List;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = SecurityQuestionRestfulController.class)
@@ -25,6 +28,19 @@ public class SecurityQuestionRestfulControllerIntegrationTests extends WebContro
             when(securityQuestionService.getAll()).thenReturn(SecurityQuestionFactory.generateMultipleDtos());
             mockMvc.perform(get("/security-questions"))
                     .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("returns with the security questions formatted in the json response body")
+        public void returnsWithTheSecurityQuestionsFormattedInTheJsonResponseBody() throws Exception {
+            List<SecurityQuestionDto> expectedSecurityQuestions = SecurityQuestionFactory.generateMultipleDtos();
+            SecurityQuestionDto firstSecurityQuestionDto = expectedSecurityQuestions.get(0);
+            when(securityQuestionService.getAll()).thenReturn(expectedSecurityQuestions);
+            mockMvc.perform(get("/security-questions"))
+                    .andExpect(jsonPath("$[0].id").exists())
+                    .andExpect(jsonPath("$[0].id").value(firstSecurityQuestionDto.getId()))
+                    .andExpect(jsonPath("$[0].question").exists())
+                    .andExpect(jsonPath("$[0].question").value(firstSecurityQuestionDto.getQuestion()));
         }
     }
 }
