0069d8141a544edd17d67045739773d55b073f43
Brendan Tierney <tiernebre@gmail.com>
2020-06-06 21:17:21 -0600

    fix db hydration

---
+++ src/main/resources/db/migration/V2020_06_06_01__Add_default_security_questions.sql
@@ -0,0 +1,5 @@
+INSERT INTO security_questions (question) VALUES
+    ('What is your favorite sports team?'),
+    ('What is your favorite video game?'),
+    ('What is your favorite sport to participate in?'),
+    ('What is your favorite video game console?');
