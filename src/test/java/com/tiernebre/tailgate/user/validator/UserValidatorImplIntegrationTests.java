94999a91cdb9ba8032563e16069e15427a553e9d
Brendan Tierney <tiernebre@gmail.com>
2020-06-08 22:00:36 -0600

    add test for non-existent security questions

--- src/test/java/com/tiernebre/tailgate/user/validator/UserValidatorImplIntegrationTests.java
+++ src/test/java/com/tiernebre/tailgate/user/validator/UserValidatorImplIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.tiernebre.tailgate.user.validator;
 
+import com.tiernebre.tailgate.security_questions.SecurityQuestionService;
 import com.tiernebre.tailgate.test.SpringIntegrationTestingSuite;
 import com.tiernebre.tailgate.user.dto.CreateUserRequest;
 import com.tiernebre.tailgate.user.dto.CreateUserSecurityQuestionRequest;
@@ -12,17 +13,26 @@ import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.testcontainers.shaded.com.google.common.collect.ImmutableList;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 import static com.tiernebre.tailgate.test.ValidatorTestUtils.assertThatValidationInvalidatedCorrectly;
+import static com.tiernebre.tailgate.user.validator.UserValidatorImpl.NON_EXISTENT_SECURITY_QUESTIONS_ERROR_MESSAGE;
 import static com.tiernebre.tailgate.user.validator.UserValidatorImpl.NULL_CREATE_USER_REQUEST_ERROR_MESSAGE;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 public class UserValidatorImplIntegrationTests extends SpringIntegrationTestingSuite {
     private static final String STRONG_PASSWORD = "Strong_Password_12345!";
+
+    @MockBean
+    private SecurityQuestionService securityQuestionService;
 
     @Autowired
     private UserValidator userValidator;
@@ -184,6 +194,25 @@ public class UserValidatorImplIntegrationTests extends SpringIntegrationTestingS
                     createUserRequest,
                     InvalidUserException.class,
                     "securityQuestions must not be empty"
+            );
+        }
+
+        @Test
+        @DisplayName("validates that the security question ids provided must all exist")
+        void testValidExistenceOfSecurityQuestionIds() {
+            Long securityQuestionId = new Random().nextLong();
+            List<CreateUserSecurityQuestionRequest> createUserSecurityQuestionRequests = ImmutableList.of(
+                    CreateUserSecurityQuestionRequest.builder().id(securityQuestionId).answer(UUID.randomUUID().toString()).build()
+            );
+            CreateUserRequest createUserRequest = CreateUserRequest.builder()
+                    .securityQuestions(createUserSecurityQuestionRequests)
+                    .build();
+            when(securityQuestionService.allExistWithIds(eq(Collections.singleton(securityQuestionId)))).thenReturn(false);
+            assertThatValidationInvalidatedCorrectly(
+                    userValidator,
+                    createUserRequest,
+                    InvalidUserException.class,
+                    NON_EXISTENT_SECURITY_QUESTIONS_ERROR_MESSAGE
             );
         }
     }
