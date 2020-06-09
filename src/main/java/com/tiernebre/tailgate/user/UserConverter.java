51c652b32cd291e76f68b64ebdb3d923fc56e045
Brendan Tierney <tiernebre@gmail.com>
2020-06-08 20:50:47 -0600

    Allow Repositories to Use Create Requests (#34)
    * make repositories respect create requests
    
    * clean up user converter stuff now that repos take in create requests

--- src/main/java/com/tiernebre/tailgate/user/UserConverter.java
+++ src/main/java/com/tiernebre/tailgate/user/UserConverter.java
@@ -1,9 +1,9 @@
 package com.tiernebre.tailgate.user;
 
-import com.tiernebre.tailgate.converter.CreatableConverter;
+import com.tiernebre.tailgate.converter.Converter;
 
 /**
  * Provides conversion operations between the User data types.
  */
-public interface UserConverter extends CreatableConverter<UserDto, UserEntity, CreateUserRequest> {
+public interface UserConverter extends Converter<UserDto, UserEntity> {
 }
