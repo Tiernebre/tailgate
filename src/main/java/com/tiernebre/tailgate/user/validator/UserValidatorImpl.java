e1e8be37dc9f96db3ecd37a20a6fad647e954efc
Brendan Tierney <tiernebre@gmail.com>
2020-06-10 20:28:19 -0600

    test security question setup

--- src/main/java/com/tiernebre/tailgate/user/validator/UserValidatorImpl.java
+++ src/main/java/com/tiernebre/tailgate/user/validator/UserValidatorImpl.java
@@ -17,14 +17,17 @@ public class UserValidatorImpl extends BaseValidator implements UserValidator {
     static final String NON_EXISTENT_SECURITY_QUESTIONS_ERROR_MESSAGE = "Some of the security question ids provided do not exist.";
 
     private final UserPasswordValidator passwordValidator;
+    private final UserSecurityQuestionValidator securityQuestionValidator;
 
     @Autowired
     public UserValidatorImpl(
             Validator validator,
-            UserPasswordValidator passwordValidator
+            UserPasswordValidator passwordValidator,
+            UserSecurityQuestionValidator securityQuestionValidator
     ) {
         super(validator);
         this.passwordValidator = passwordValidator;
+        this.securityQuestionValidator = securityQuestionValidator;
     }
 
     @Override
@@ -32,7 +35,9 @@ public class UserValidatorImpl extends BaseValidator implements UserValidator {
         Objects.requireNonNull(createUserRequest, NULL_CREATE_USER_REQUEST_ERROR_MESSAGE);
         Set<String> errorsFound = validateCommon(createUserRequest);
         Set<String> passwordErrors = passwordValidator.validate(createUserRequest.getPassword(), createUserRequest.getConfirmationPassword());
+        Set<String> securityQuestionErrors = securityQuestionValidator.validate(createUserRequest);
         errorsFound.addAll(passwordErrors);
+        errorsFound.addAll(securityQuestionErrors);
         if (CollectionUtils.isNotEmpty(errorsFound)) {
             throw new InvalidUserException(errorsFound);
         }
