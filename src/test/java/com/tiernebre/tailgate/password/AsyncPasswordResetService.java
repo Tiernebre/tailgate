f0ee67dd1de7be2bd3717054960ad6d8e23e707c
Brendan Tierney <tiernebre@gmail.com>
2020-06-11 22:29:39 -0600

    final dependency

--- src/test/java/com/tiernebre/tailgate/password/AsyncPasswordResetService.java
+++ src/test/java/com/tiernebre/tailgate/password/AsyncPasswordResetService.java
@@ -1,13 +1,15 @@
 package com.tiernebre.tailgate.password;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AsyncPasswordResetService implements PasswordResetService {
-    private PasswordResetService passwordResetService;
+    private final PasswordResetService passwordResetService;
 
+    @Async
     @Override
     public void createOne(PasswordResetRequest passwordResetRequest) {
         passwordResetService.createOne(passwordResetRequest);
