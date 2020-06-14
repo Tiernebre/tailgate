fed7ef27b55083ed3ecbf208678ab2922cccfa95
Brendan Tierney <tiernebre@gmail.com>
2020-06-14 17:01:33 -0600

    repository renaming

--- src/main/java/com/tiernebre/tailgate/user/repository/UserPasswordJooqRepository.java
+++ src/main/java/com/tiernebre/tailgate/user/repository/UserPasswordJooqRepository.java
@@ -2,7 +2,7 @@ package com.tiernebre.tailgate.user.repository;
 
 public class UserPasswordJooqRepository implements UserPasswordRepository {
     @Override
-    public void updatePasswordForOneWithEmailAndNonExpiredPasswordResetToken(String password, String email, String passwordResetToken) {
+    public void updateOneWithEmailAndNonExpiredResetToken(String password, String email, String resetToken) {
 
     }
 }
--- src/main/java/com/tiernebre/tailgate/user/repository/UserPasswordRepository.java
+++ src/main/java/com/tiernebre/tailgate/user/repository/UserPasswordRepository.java
@@ -8,7 +8,7 @@ public interface UserPasswordRepository {
      * Updates a user's password using a given reset token and a user email.
      * @param password The password to update to.
      * @param email The email of the user to update
-     * @param passwordResetToken The password reset token also tied to a password reset request.
+     * @param resetToken The password reset token also tied to a password reset request.
      */
-    void updatePasswordForOneWithEmailAndNonExpiredPasswordResetToken(String password, String email, String passwordResetToken);
+    void updateOneWithEmailAndNonExpiredResetToken(String password, String email, String resetToken);
 }
