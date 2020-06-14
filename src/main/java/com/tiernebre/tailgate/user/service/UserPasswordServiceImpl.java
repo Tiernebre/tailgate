5102c32434a240702830384f36d424063a016140
Brendan Tierney <tiernebre@gmail.com>
2020-06-14 15:47:58 -0600

    password service implementation

---
+++ src/main/java/com/tiernebre/tailgate/user/service/UserPasswordServiceImpl.java
@@ -0,0 +1,10 @@
+package com.tiernebre.tailgate.user.service;
+
+import com.tiernebre.tailgate.user.dto.ResetTokenUpdatePasswordRequest;
+
+public class UserPasswordServiceImpl implements UserPasswordService {
+    @Override
+    public void updateOneUsingResetToken(String resetToken, ResetTokenUpdatePasswordRequest updatePasswordRequest) {
+
+    }
+}
