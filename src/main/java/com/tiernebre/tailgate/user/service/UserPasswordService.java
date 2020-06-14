08743cac3cf143d3ea4f66172d15eb4ee501557e
Brendan Tierney <tiernebre@gmail.com>
2020-06-14 15:41:37 -0600

    interface updates

--- src/main/java/com/tiernebre/tailgate/user/service/UserPasswordService.java
+++ src/main/java/com/tiernebre/tailgate/user/service/UserPasswordService.java
@@ -1,9 +1,12 @@
 package com.tiernebre.tailgate.user.service;
 
+import com.tiernebre.tailgate.user.dto.ResetTokenUpdatePasswordRequest;
+
 public interface UserPasswordService {
     /**
      * Updates a user password given a reset token
      * @param resetToken The reset token assigned from a forgotten password request.
+     * @param updatePasswordRequest The request to handle a password reset.
      */
-    void updateOneWithResetToken(String resetToken);
+    void updateOneUsingResetToken(String resetToken, ResetTokenUpdatePasswordRequest updatePasswordRequest);
 }
