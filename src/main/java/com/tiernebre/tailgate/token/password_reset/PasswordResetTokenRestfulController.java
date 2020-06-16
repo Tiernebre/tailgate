7e02bde86e1a3670654cfb1463e3855636ef325b
Brendan Tierney <tiernebre@gmail.com>
2020-06-15 20:59:28 -0600

    Implement Password Reset Token Update Controller (#53)
    * setup controller
    
    * setup tests
    
    * working integration test

---
+++ src/main/java/com/tiernebre/tailgate/token/password_reset/PasswordResetTokenRestfulController.java
@@ -0,0 +1,26 @@
+package com.tiernebre.tailgate.token.password_reset;
+
+import com.tiernebre.tailgate.user.dto.ResetTokenUpdatePasswordRequest;
+import com.tiernebre.tailgate.user.exception.InvalidPasswordResetTokenException;
+import com.tiernebre.tailgate.user.exception.InvalidUpdatePasswordRequestException;
+import com.tiernebre.tailgate.user.exception.UserNotFoundForPasswordUpdateException;
+import com.tiernebre.tailgate.user.service.UserPasswordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("password-reset-tokens")
+@RequiredArgsConstructor
+public class PasswordResetTokenRestfulController {
+    private final UserPasswordService userPasswordService;
+
+    @PatchMapping("/{passwordResetToken}/password")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updatePasswordUsingResetToken(
+            @PathVariable String passwordResetToken,
+            @RequestBody ResetTokenUpdatePasswordRequest request
+    ) throws UserNotFoundForPasswordUpdateException, InvalidPasswordResetTokenException, InvalidUpdatePasswordRequestException {
+        userPasswordService.updateOneUsingResetToken(passwordResetToken, request);
+    }
+}
---
+++ src/test/java/com/tiernebre/tailgate/token/password_reset/PasswordResetTokenRestfulControllerTests.java
@@ -0,0 +1,42 @@
+package com.tiernebre.tailgate.token.password_reset;
+
+import com.tiernebre.tailgate.user.dto.ResetTokenUpdatePasswordRequest;
+import com.tiernebre.tailgate.user.exception.InvalidPasswordResetTokenException;
+import com.tiernebre.tailgate.user.exception.InvalidUpdatePasswordRequestException;
+import com.tiernebre.tailgate.user.exception.UserNotFoundForPasswordUpdateException;
+import com.tiernebre.tailgate.user.service.UserPasswordService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class PasswordResetTokenRestfulControllerTests {
+    @InjectMocks
+    private PasswordResetTokenRestfulController passwordResetTokenRestfulController;
+
+    @Mock
+    private UserPasswordService userPasswordService;
+
+    @Nested
+    @DisplayName("updatePasswordUsingResetToken")
+    class UpdatePasswordUsingResetTokenTests {
+        @Test
+        @DisplayName("passes through the request and params properly")
+        void passesThroughTheRequestAndParamsProperly() throws InvalidUpdatePasswordRequestException, InvalidPasswordResetTokenException, UserNotFoundForPasswordUpdateException {
+            String resetToken = UUID.randomUUID().toString();
+            ResetTokenUpdatePasswordRequest resetTokenUpdatePasswordRequest = ResetTokenUpdatePasswordRequestFactory.generateOneDto();
+            passwordResetTokenRestfulController.updatePasswordUsingResetToken(resetToken, resetTokenUpdatePasswordRequest);
+            verify(userPasswordService, times(1)).updateOneUsingResetToken(eq(resetToken), eq(resetTokenUpdatePasswordRequest));
+        }
+    }
+}
