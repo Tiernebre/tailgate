a203e3de8b6a70ded46128c2131d6769c22517af
Brendan Tierney <tiernebre@gmail.com>
2020-06-21 14:35:09 -0600

    extra provided answers handling

--- src/test/java/com/tiernebre/tailgate/user/service/UserSecurityQuestionsServiceImplTests.java
+++ src/test/java/com/tiernebre/tailgate/user/service/UserSecurityQuestionsServiceImplTests.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserSecurityQuestionsServiceImplTests {
@@ -104,6 +104,32 @@ public class UserSecurityQuestionsServiceImplTests {
                             providedAnswers
                     )
             );
+        }
+
+        @Test
+        @DisplayName("ignores extra provided answers that do not match to found answers")
+        void throwsAnInvalidErrorIfProvidedAnswersIncludedTooFewAnswers() {
+            String resetToken = UUID.randomUUID().toString();
+            String email = UUID.randomUUID().toString();
+            Map<Long, String> foundAnswers = new HashMap<>();
+            Map<Long, String> providedAnswers = new HashMap<>();
+            for (long i = 0; i < 2; i++) {
+                String foundAnswer = UUID.randomUUID().toString();
+                String providedAnswer = UUID.randomUUID().toString();
+                foundAnswers.put(i, foundAnswer);
+                providedAnswers.put(i, providedAnswer);
+                when(passwordEncoder.matches(providedAnswer, foundAnswer)).thenReturn(true);
+            }
+            providedAnswers.put(Integer.toUnsignedLong(providedAnswers.size()), UUID.randomUUID().toString());
+            when(repository.getAnswersForEmailAndResetToken(eq(email), eq(resetToken))).thenReturn(foundAnswers);
+            assertDoesNotThrow(() ->
+                    userSecurityQuestionsService.validateAnswersForUserWithEmailAndResetToken(
+                            email,
+                            resetToken,
+                            providedAnswers
+                    )
+            );
+            verify(passwordEncoder, times(foundAnswers.size())).matches(anyString(), anyString());
         }
     }
 }
