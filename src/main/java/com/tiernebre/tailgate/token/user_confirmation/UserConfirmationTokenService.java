55da6339261610a7e8318a3c2e5033c96c66024b
Brendan Tierney <tiernebre@gmail.com>
2020-06-21 20:40:38 -0600

    interface method

--- src/main/java/com/tiernebre/tailgate/token/user_confirmation/UserConfirmationTokenService.java
+++ src/main/java/com/tiernebre/tailgate/token/user_confirmation/UserConfirmationTokenService.java
@@ -10,4 +10,14 @@ public interface UserConfirmationTokenService {
      * @return The created confirmation token.
      */
     String createOneForUser(UserDto user);
+
+    /**
+     * Tries to find a confirmation token for a user. If for some
+     * reason a confirmation token does not exist for a user, it
+     * generates a brand new one and uses that instead.
+     *
+     * @param user The user to either find or create a new confirmation token for.
+     * @return The found or generated confirmation token.
+     */
+    String findOrGenerateForUser(UserDto user);
 }
