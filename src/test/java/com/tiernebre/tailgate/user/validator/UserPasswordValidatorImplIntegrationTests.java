dbffbd81f2a26498ea55e79005d3d3b4ef0e2511
Brendan Tierney <tiernebre@gmail.com>
2020-06-22 21:39:24 -0600

    more method renaming

--- src/test/java/com/tiernebre/tailgate/user/validator/UserPasswordValidatorImplIntegrationTests.java
+++ src/test/java/com/tiernebre/tailgate/user/validator/UserPasswordValidatorImplIntegrationTests.java
@@ -27,8 +27,8 @@ public class UserPasswordValidatorImplIntegrationTests extends SpringIntegration
     private UserPasswordValidatorImpl userPasswordValidator;
 
     @Nested
-    @DisplayName("validateUpdateRequest")
-    class ValidateUpdateRequestTests {
+    @DisplayName("validateResetTokenUpdateRequest")
+    class ValidateResetTokenUpdateRequestTests {
         @NullSource
         @EmptySource
         @ValueSource(strings = { " " })
