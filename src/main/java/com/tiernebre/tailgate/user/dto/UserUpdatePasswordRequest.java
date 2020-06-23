e2ee577a986c34b8ad24fd6c9fe600120cdc8bcf
Brendan Tierney <tiernebre@gmail.com>
2020-06-22 21:51:15 -0600

    re usable field on password update request

--- src/main/java/com/tiernebre/tailgate/user/dto/UserUpdatePasswordRequest.java
+++ src/main/java/com/tiernebre/tailgate/user/dto/UserUpdatePasswordRequest.java
@@ -1,12 +1,22 @@
 package com.tiernebre.tailgate.user.dto;
 
+import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 
-@Value
-@Builder
-public class UserUpdatePasswordRequest {
+@Getter
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class UserUpdatePasswordRequest extends UpdatePasswordRequest {
+    @Builder
+    public UserUpdatePasswordRequest(
+            String oldPassword,
+            String newPassword,
+            String confirmationNewPassword
+    ) {
+        super(newPassword, confirmationNewPassword);
+        this.oldPassword = oldPassword;
+    }
+
     String oldPassword;
-    String newPassword;
-    String confirmationNewPassword;
 }
