51dfd8cd59f29e1515ae3f7b7c4639f67003e5a2
Brendan Tierney <tiernebre@gmail.com>
2020-06-23 22:05:05 -0600

    test happy path

--- src/test/java/com/tiernebre/tailgate/token/password_reset/PasswordResetTokenRestfulControllerIntegrationTests.java
+++ src/test/java/com/tiernebre/tailgate/token/password_reset/PasswordResetTokenRestfulControllerIntegrationTests.java
@@ -1,5 +1,8 @@
 package com.tiernebre.tailgate.token.password_reset;
 
+import com.tiernebre.tailgate.security_questions.SecurityQuestionDto;
+import com.tiernebre.tailgate.security_questions.SecurityQuestionFactory;
+import com.tiernebre.tailgate.security_questions.SecurityQuestionService;
 import com.tiernebre.tailgate.test.WebControllerIntegrationTestSuite;
 import com.tiernebre.tailgate.user.dto.ResetTokenUpdatePasswordRequest;
 import com.tiernebre.tailgate.user.service.UserPasswordService;
@@ -9,11 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,6 +27,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PasswordResetTokenRestfulControllerIntegrationTests extends WebControllerIntegrationTestSuite {
     @MockBean
     private UserPasswordService userPasswordService;
+
+    @MockBean
+    private SecurityQuestionService securityQuestionService;
 
     @Nested
     @DisplayName("PATCH /password-reset-tokens/{passwordResetToken}/password")
@@ -37,6 +46,20 @@ public class PasswordResetTokenRestfulControllerIntegrationTests extends WebCont
                         .content(objectMapper.writeValueAsString(resetTokenUpdatePasswordRequest))
                         .contentType(APPLICATION_JSON_VALUE)
             ).andExpect(status().isNoContent());
+        }
+    }
+
+    @Nested
+    @DisplayName("GET /password-reset-tokens/{passwordResetToken}/security-questions")
+    class GetPasswordResetTokenSecurityQuestions {
+        @Test
+        @DisplayName("returns with 200 OK status")
+        void returnsWith204NoContentStatus() throws Exception {
+            String resetToken = UUID.randomUUID().toString();
+            String uri = String.format("/password-reset-tokens/%s/security-questions", resetToken);
+            List<SecurityQuestionDto> expectedSecurityQuestions = SecurityQuestionFactory.generateMultipleDtos();
+            when(securityQuestionService.getAllForPasswordResetToken(eq(resetToken))).thenReturn(expectedSecurityQuestions);
+            mockMvc.perform(get(uri)).andExpect(status().isOk());
         }
     }
 }
