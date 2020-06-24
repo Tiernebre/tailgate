929c99c394bd443ee354eb216858cecbac4e7c26
Brendan Tierney <tiernebre@gmail.com>
2020-06-23 21:21:30 -0600

    remove emptying of filters

--- src/test/java/com/tiernebre/tailgate/test/WebControllerIntegrationTestSuite.java
+++ src/test/java/com/tiernebre/tailgate/test/WebControllerIntegrationTestSuite.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.web.servlet.MockMvc;
 
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc()
 @MockBeans({
         @MockBean(AccessTokenProvider.class)
 })
