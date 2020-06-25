439823e9e015595581b33beb31c761a2bbddca47
Brendan Tierney <tiernebre@gmail.com>
2020-06-24 20:48:54 -0600

    attempt at a postgres trigger

--- src/main/resources/db/migration/V2020_06_24_02__Create_user_password_updates.sql
+++ src/main/resources/db/migration/V2020_06_24_02__Create_user_password_updates.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS audit.user_password_updates (
+    id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES public.users (id),
     updated_at TIMESTAMP NOT NULL DEFAULT now()
 ):
---
+++ src/main/resources/db/migration/V2020_06_24_03__Add_user_password_update_trigger.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION audit_user_password_update()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+	IF NEW.password <> OLD.password THEN
+		 INSERT INTO audit.user_password_updates(user_id)
+		 VALUES(OLD.id);
+	END IF;
+
+	RETURN NEW;
+END;
+$BODY$;
+
+CREATE TRIGGER user_password_changes
+  BEFORE UPDATE
+  ON public.users
+  FOR EACH ROW
+  EXECUTE PROCEDURE audit_user_password_update();
