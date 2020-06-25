6f4f6944f249ab42b048600b1591338c9e73a4cd
Brendan Tierney <tiernebre@gmail.com>
2020-06-24 20:49:22 -0600

    fix syntax error

--- src/main/resources/db/migration/V2020_06_24_02__Create_user_password_updates.sql
+++ src/main/resources/db/migration/V2020_06_24_02__Create_user_password_updates.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS audit.user_password_updates (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES public.users (id),
     updated_at TIMESTAMP NOT NULL DEFAULT now()
-):
+);
