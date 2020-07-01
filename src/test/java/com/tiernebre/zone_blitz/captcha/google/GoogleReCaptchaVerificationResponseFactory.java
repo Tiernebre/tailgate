55da84e6ac2afc91a97d7dd9f2ed102b06745f78
Brendan Tierney <tiernebre@gmail.com>
2020-06-30 22:26:13 -0600

    test false response

--- src/test/java/com/tiernebre/zone_blitz/captcha/google/GoogleReCaptchaVerificationResponseFactory.java
+++ src/test/java/com/tiernebre/zone_blitz/captcha/google/GoogleReCaptchaVerificationResponseFactory.java
@@ -1,15 +1,17 @@
 package com.tiernebre.zone_blitz.captcha.google;
 
-import com.tiernebre.zone_blitz.captcha.google.GoogleReCaptchaVerificationResponse;
-
 import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.UUID;
 
 public class GoogleReCaptchaVerificationResponseFactory {
     public static GoogleReCaptchaVerificationResponse generateOneDto() {
+        return generateOneDto(true);
+    }
+
+    public static GoogleReCaptchaVerificationResponse generateOneDto(boolean success) {
         return GoogleReCaptchaVerificationResponse.builder()
-                .success(true)
+                .success(success)
                 .challengeTimestamp(ZonedDateTime.now())
                 .hostname(UUID.randomUUID().toString())
                 .errorCodes(Collections.emptySet())
--- src/test/java/com/tiernebre/zone_blitz/captcha/google/GoogleReCaptchaVerifierTests.java
+++ src/test/java/com/tiernebre/zone_blitz/captcha/google/GoogleReCaptchaVerifierTests.java
@@ -1,5 +1,6 @@
 package com.tiernebre.zone_blitz.captcha.google;
 
+import com.tiernebre.zone_blitz.captcha.CaptchaIsNotValidException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.when;
@@ -45,6 +47,26 @@ public class GoogleReCaptchaVerifierTests {
                     eq(captchaToken)
             )).thenReturn(mockedGoogleResponse);
             assertDoesNotThrow(() -> googleReCaptchaVerifier.verify(captchaToken));
+        }
+
+        @Test
+        @DisplayName("throws not valid captcha error if the captcha token is invalid")
+        void throwsNotValidCaptchaErrorIfTheCaptchaTokenIsInvalid() {
+            String secret = UUID.randomUUID().toString();
+            when(googleReCaptchaConfigurationProperties.getSecret()).thenReturn(secret);
+            String captchaToken = UUID.randomUUID().toString();
+            GoogleReCaptchaVerificationResponse mockedGoogleResponse = GoogleReCaptchaVerificationResponseFactory.generateOneDto(false);
+            when(googleReCaptchaRestTemplate.postForObject(
+                    eq("/siteverify?secret={secret}&response={response}"),
+                    isNull(),
+                    eq(GoogleReCaptchaVerificationResponse.class),
+                    eq(secret),
+                    eq(captchaToken)
+            )).thenReturn(mockedGoogleResponse);
+            assertThrows(
+                    CaptchaIsNotValidException.class,
+                    () -> googleReCaptchaVerifier.verify(captchaToken)
+            );
         }
     }
 }
