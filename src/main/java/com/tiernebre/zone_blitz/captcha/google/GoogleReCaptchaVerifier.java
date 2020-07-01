4ec33ad98d6d033d6748293fdb4e3bf8b0e95f6f
Brendan Tierney <tiernebre@gmail.com>
2020-06-30 21:43:27 -0600

    google implementation

---
+++ src/main/java/com/tiernebre/zone_blitz/captcha/google/GoogleReCaptchaVerifier.java
@@ -0,0 +1,11 @@
+package com.tiernebre.zone_blitz.captcha.google;
+
+import com.tiernebre.zone_blitz.captcha.CaptchaIsNotValidException;
+import com.tiernebre.zone_blitz.captcha.CaptchaVerifier;
+
+public class GoogleReCaptchaVerifier implements CaptchaVerifier {
+    @Override
+    public void verify(String captchaToken) throws CaptchaIsNotValidException {
+
+    }
+}
