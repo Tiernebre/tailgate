bfc1e1c6943ce4bad2764772415777b22f86f164
Brendan Tierney <tiernebre@gmail.com>
2020-06-30 22:03:07 -0600

    fix name on compose file

--- src/main/java/com/tiernebre/zone_blitz/captcha/google/GoogleReCaptchaConfigurationProperties.java
+++ src/main/java/com/tiernebre/zone_blitz/captcha/google/GoogleReCaptchaConfigurationProperties.java
@@ -2,9 +2,11 @@ package com.tiernebre.zone_blitz.captcha.google;
 
 import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties(prefix = "google.recaptcha")
 @Value
+@ConstructorBinding
 public class GoogleReCaptchaConfigurationProperties {
     String url;
 }
