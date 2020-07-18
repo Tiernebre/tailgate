84cb520796c19b64f3e4627a4f7c365c61e676a2
Brendan Tierney <tiernebre@gmail.com>
2020-07-17 23:16:02 -0600

    setup secure random fingerprint generator

--- src/main/java/com/tiernebre/zone_blitz/token/access/fingerprint/AccessTokenSecureRandomFingerprintGenerator.java
+++ src/main/java/com/tiernebre/zone_blitz/token/access/fingerprint/AccessTokenSecureRandomFingerprintGenerator.java
@@ -1,8 +1,21 @@
 package com.tiernebre.zone_blitz.token.access.fingerprint;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.SecureRandom;
+
+@Component
+@RequiredArgsConstructor
 public class AccessTokenSecureRandomFingerprintGenerator implements AccessTokenFingerprintGenerator {
+    private final static int SIZE_OF_FINGERPRINT_IN_BYTES = 50;
+    private final SecureRandom secureRandom;
+
     @Override
     public String generateOne() {
-        return null;
+        byte[] randomFingerprint = new byte[SIZE_OF_FINGERPRINT_IN_BYTES];
+        secureRandom.nextBytes(randomFingerprint);
+        return DatatypeConverter.printHexBinary(randomFingerprint);
     }
 }
