35455af05b3d9c258b9f312dd58c10aae5cfd07e
Brendan Tierney <tiernebre@gmail.com>
2020-07-20 21:55:44 -0600

    cleaner logging

--- src/main/java/com/tiernebre/zone_blitz/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/tiernebre/zone_blitz/exception/GlobalControllerExceptionHandler.java
@@ -28,13 +28,8 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleGlobalError(Exception globalError) {
-        String errorLog;
-        if (globalError instanceof RuntimeException) {
-            errorLog = "Encountered RuntimeException: ";
-        } else {
-            errorLog = "Encountered unhandled exception error: ";
-        }
-        log.error(errorLog, globalError);
+        String errorLog = globalError instanceof RuntimeException ? "Encountered RuntimeException" : "Encountered unhandled exception error";
+        log.error(errorLog + ": ", globalError);
         log.info("Proceeding to respond with generic error message for a globally encountered error.");
         return ErrorResponse.of(GENERIC_ERROR_MESSAGE);
     }
