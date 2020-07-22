a6d1eb922abc070bd9e45efbe233ee5500e8d225
Brendan Tierney <tiernebre@gmail.com>
2020-07-21 20:57:53 -0600

    Safer Generic Error / Exception Handling (#75)
    * unauthorized exception
    
    * global exception handler
    
    * add unit test for invalid exception
    
    * global error handling
    
    * handle runtime and normal exceptions
    
    * cleaner logging
    
    * fix 403 errors
    
    * remove redundant rest controller
    
    * hacky way to make access denied exceptions work again
    
    * setup access denied exceptions again
    
    * test more scalable set solution
    
    * handle other exceptions gracefully
    
    * Revert changes on jwt auth filter

--- src/main/java/com/tiernebre/zone_blitz/exception/ErrorResponse.java
+++ src/main/java/com/tiernebre/zone_blitz/exception/ErrorResponse.java
@@ -3,10 +3,17 @@ package com.tiernebre.zone_blitz.exception;
 import lombok.Builder;
 import lombok.Value;
 
+import java.util.Collections;
 import java.util.Set;
 
 @Value
 @Builder
 public class ErrorResponse {
+    public static ErrorResponse of(String message) {
+        return ErrorResponse.builder()
+                .errors(Collections.singleton(message))
+                .build();
+    }
+
     Set<String> errors;
 }
--- src/main/java/com/tiernebre/zone_blitz/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/tiernebre/zone_blitz/exception/GlobalControllerExceptionHandler.java
@@ -2,14 +2,21 @@ package com.tiernebre.zone_blitz.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.ServletException;
+
+import static com.tiernebre.zone_blitz.exception.GlobalControllerExceptionHandlerConfiguration.EXCEPTIONS_TO_LET_SPRING_HANDLE;
 
 @ControllerAdvice
-@RestController
 @Slf4j
 public class GlobalControllerExceptionHandler {
+    static final String GENERIC_ERROR_MESSAGE = "An error on the server occurred. Please retry again or double check your information.";
+
     @ExceptionHandler(InvalidException.class)
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleInvalidException(InvalidException invalidException) {
         return ErrorResponse.builder()
@@ -18,14 +25,25 @@ public class GlobalControllerExceptionHandler {
     }
 
     @ExceptionHandler(NotFoundException.class)
-    @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public void handleInvalidException() {
     }
 
-    @ExceptionHandler(UnauthorizedException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public void handleUnauthorizedException() {
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public void handleAccessDeniedException() {
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleGlobalError(Exception globalError) throws Exception {
+        boolean exceptionShouldBeHandledBySpring = globalError instanceof ServletException || EXCEPTIONS_TO_LET_SPRING_HANDLE.contains(globalError.getClass());
+        if (exceptionShouldBeHandledBySpring) {
+            throw globalError;
+        }
+        String errorLog = globalError instanceof RuntimeException ? "Encountered RuntimeException" : "Encountered unhandled exception error";
+        log.error(errorLog + ": ", globalError);
+        log.info("Proceeding to respond with generic error message for a globally encountered error.");
+        return ErrorResponse.of(GENERIC_ERROR_MESSAGE);
     }
 }
