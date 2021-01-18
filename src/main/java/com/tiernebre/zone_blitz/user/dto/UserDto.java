3849fadac8bc231aec22a03266e1e3881176fafd
Brendan Tierney <tiernebre@gmail.com>
2021-01-17 17:22:26 -0700

    make user dto not a representational model

--- src/main/java/com/tiernebre/zone_blitz/user/dto/UserDto.java
+++ src/main/java/com/tiernebre/zone_blitz/user/dto/UserDto.java
@@ -2,14 +2,11 @@ package com.tiernebre.zone_blitz.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Value;
-import org.springframework.hateoas.RepresentationModel;
 
 @Value
-@EqualsAndHashCode(callSuper = true)
 @Builder
-public class UserDto extends RepresentationModel<UserDto> {
+public class UserDto {
     Long id;
     String email;
     @JsonIgnore
